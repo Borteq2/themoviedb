1ab23a9a8c42eb5ef93f3a319b99ca2df4ddc73a
Borteq2 <borteq2@yandex.ru>
2023-06-02 12:12:40 +0300

    add YT videoplayer for trailers

--- lib/ui/widgets/movie_trailer/movie_trailer_widget.dart
+++ lib/ui/widgets/movie_trailer/movie_trailer_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class MovieTrailerWidget extends StatefulWidget {
   final String youTubeKey;
@@ -13,8 +14,37 @@ class MovieTrailerWidget extends StatefulWidget {
 }
 
 class _MovieTrailerWidgetState extends State<MovieTrailerWidget> {
+  late final YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      initialVideoId: widget.youTubeKey,
+      flags: const YoutubePlayerFlags(
+        autoPlay: true,
+        mute: false,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final player = YoutubePlayer(
+      controller: _controller,
+      showVideoProgressIndicator: true,
+    );
+    return YoutubePlayerBuilder(
+        player: player,
+        builder: (context, player) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Trailer'),
+            ),
+            body: Center(
+              child: player,
+            ),
+          );
+        });
   }
 }
